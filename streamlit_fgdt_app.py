6b2f25e9cb4611f2c649c7a35ff0519155dd12a3
fervl017 <133587826+fervl017@users.noreply.github.com>
2025-07-28 10:37:37 +0200

    Update streamlit_fgdt_app.py

--- streamlit_fgdt_app.py
+++ streamlit_fgdt_app.py
@@ -31,6 +31,6 @@ if pd.notna(df_models[selected_feature].min()) and pd.notna(df_models[selected_f
    # Mostrar resultados
    st.subheader("Matching FortiGate Models")
    if not filtered_df.empty:
-       st.dataframe(filtered_df)
+        st.table(filtered_df.reset_index().rename(columns={"index": "Feature"}))
    else:
        st.warning("No hay modelos que cumplan con el criterio.")
